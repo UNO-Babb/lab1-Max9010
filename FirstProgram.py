ff6eb338e7d714f637ddd16b1b22d25b9e10b14c
Max9010 <maxwellperry@unomaha.edu>
2024-08-28 16:42:14 +0000

    Finished FirstProgram and MadLib

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,21 +1,23 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name:Max Perry
+#Date:8/28/2024
+#Assignment:FristProgram
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("hello")
   #Ask for the user's name
-
+  name=input("What is your name?")
   #Use the user's name in the program.
-
+  input("How are you today "+name+"?")
   #Ask the user for their age.
-
+  age=input("How old are you "+name+"?")
+  age=int(age)
+  birthyear=2024-age
   #Tell the user what year they were born in.
+  print (name,", you were born in ",birthyear)
   #Assume that they have not had their birthday yet this year.
-
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
--- MadLib.py
+++ MadLib.py
@@ -1,14 +1,23 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name:Max Perry
+#Date:8/28/2024
+#Assignment:MadLib
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  noun1= input("Please enter a noun. ")
+  verb1= input("Please enter a verb. ")
+  animal1= input("Please enter an animal. ")
+  verb2= input("Please enter a verb. ")
+  adjective1= input("Please enter a adjective. ")
+  noun2= input("Please enter a noun. ")
   #Print the story with the user supplied words.
-
+  print("Once upon a time there was a " + noun1 + ".")
+  print("It was very a scary " + noun1 + " that loved to " + verb1 + ".")
+  print("One day the " + noun1 + " was stumbled upon by a small little " +animal1+".")
+  print("The "+animal1+" laughed, played, and "+verb2+"ed.")
+  print("I can't thing of anything else" ,adjective1,noun2+".")
 
 
 #Call the main function if this is the file being run.
